a6f236328493b1929c46a901dc30290277fcc29b
Matt Raykowski <matt.raykowski@gmail.com>
2019-08-05 11:13:34 -0500

    [#4] Added initial package information and Travis config.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule HamRadio.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      # Package Information
+      name: "HamRadio",
+      source_url: "https://github.com/hamhub/ham_radio_ex",
+      description: description(),
+      package: package()
     ]
   end
 
@@ -25,6 +30,19 @@ defmodule HamRadio.MixProject do
       {:mix_test_watch, "~> 0.8", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp description() do
+    "A general purpose library of amateur radio utilities and types."
+  end
+
+  defp package() do
+    [
+      name: "ham_radio",
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE* CHANGELOG*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/hamhub/ham_radio_ex"}
     ]
   end
 end
